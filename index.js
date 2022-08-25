cb6ba8bbecc9dd70e020f3b562b70c6639030900
HPollefrone <henrique-ps3@hotmail.com>
2022-08-25 17:39:03 -0300

    Implementa resultados da leitura
    Co-authored-by: maysilsantos <maysilsantos@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const JSZip = require("jszip");
 fs.readFile("teste.zip", function(err, data) {
     if (err) throw err;
     JSZip.loadAsync(data).then(function (zip) {
-        const xmlZip = zip
-        console.log('Conteudo ziado - >', xmlZip)
+        const res = zip
+        console.log(res.files)
+        res.files
+            
+        });
+        //console.log(res.nfeProc.NFe[0].infNFe[0].emit[0])
+        //CNPJ CEDENTE - EMITENTE
+        //console.log(res.nfeProc.NFe[0].infNFe[0].emit[0].CNPJ[0])
+        //CNPJ SACADO - DET
+        //console.log(res.nfeProc.NFe[0].infNFe[0].dest[0].CNPJ[0])            
+        //DATA VENCIMENTO
+        //console.log(res.nfeProc.NFe[0].infNFe[0].cobr[0].dup[0].dVenc[0])
+        //VALOR DA NOTA
+        //console.log(res.nfeProc.NFe[0].infNFe[0].cobr[0].dup[0].vDup[0])           
+        //DUPLICATA == CÓDIGO DO BOLETO 
+        //console.log(res.nfeProc.NFe[0].infNFe[0].cobr[0].dup[0].nDup[0])
         
 });
 
-});
